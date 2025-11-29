e7ddec0a0d844f4dfc91a0bc8b3684b1bd3a5a63
JELmzabi <elmzabijawad@gmail.com>
2025-11-29 01:02:53 +0100

    fix articles stock ui responsive

--- resources/js/Pages/Stock/ArticlesStocks/Index.vue
+++ resources/js/Pages/Stock/ArticlesStocks/Index.vue
@@ -56,25 +56,25 @@ function applyFilters() {
       <div class="bg-white p-6 rounded-xl shadow-sm border border-gray-200">
         <h3 class="text-lg font-semibold text-gray-800 mb-4">Filtrer les articles</h3>
 
-        <div class="grid grid-cols-1 md:grid-cols-5 gap-4 mb-4">
-          <div class="md:col-span-2">
-            <label class="block text-sm font-medium text-gray-700 mb-2">Recherche</label>
-            <div class="relative">
-              <input v-model="filters.search" type="text" placeholder="Référence ou désignation..."
-                class="w-full border border-gray-300 rounded-lg pl-10 pr-3 py-2 focus:ring-blue-500 focus:border-blue-500" />
-              <MagnifyingGlassIcon class="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
+        <div class="flex flex-col lg:flex-row gap-4">
+            <div class="w-full">
+              <label class="block text-sm font-medium text-gray-700 mb-2">Recherche</label>
+              <div class="relative">
+                <input v-model="filters.search" type="text" placeholder="Référence ou désignation..."
+                  class="w-full border border-gray-300 rounded-lg pl-10 pr-3 py-2 focus:ring-blue-500 focus:border-blue-500" />
+                <MagnifyingGlassIcon class="absolute left-3 top-2.5 h-5 w-5 text-gray-400" />
+              </div>
             </div>
-          </div>
 
-          <div class="md:col-span-2">
-            <label class="block text-sm font-medium text-gray-700 mb-2">Categorie</label>
-            <select v-model="filters.categorie" class="w-full border border-gray-300 rounded-lg p-2">
-              <option value="">Toutes les catégories</option>
-              <option v-for="cat in categories" :key="cat.id" :value="cat.id">{{ cat.nom }}</option>
-            </select>
-          </div>
+            <div class="w-full">
+              <label class="block text-sm font-medium text-gray-700 mb-2">Categorie</label>
+              <select v-model="filters.categorie" class="w-full border border-gray-300 rounded-lg p-2">
+                <option value="">Toutes les catégories</option>
+                <option v-for="cat in categories" :key="cat.id" :value="cat.id">{{ cat.nom }}</option>
+              </select>
+            </div>
 
-          <div class="mt-5 flex flex-col sm:flex-row justify-end gap-3">
+          <div class="flex gap-3 ms-auto items-end">
             <button @click="resetFilters"
               class="px-4 py-2 border border-gray-300 rounded-lg text-gray-700 hover:bg-gray-50 transition-all">
               Réinitialiser
